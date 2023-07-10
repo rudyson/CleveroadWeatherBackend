fab61a358e202e35cb37390b7df59eefeb035f77
Ruslan Diadiushkin <contact@xnrudyson.anonaddy.me>
2023-07-10 12:19:28 +0300

    Added IWeatherRepository description

--- Repositories/IWeatherRepository.cs
+++ Repositories/IWeatherRepository.cs
@@ -4,6 +4,16 @@ namespace CleveroadWeatherBackend.Repositories;
 
 public interface IWeatherRepository
 {
+    /// <summary>
+    /// Gets weather forecast for 5 days in the provided city
+    /// </summary>
+    /// <param name="name">City name</param>
+    /// <returns></returns>
     public Task<IEnumerable<WeatherForecastDto>?> GetForecast5Days(string name);
+    /// <summary>
+    /// Gets current weather in the provided city
+    /// </summary>
+    /// <param name="name">City name</param>
+    /// <returns></returns>
     public Task<WeatherCurrentDto?> GetCurrentWeather(string name);
 }
